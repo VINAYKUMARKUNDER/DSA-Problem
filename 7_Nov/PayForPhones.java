3fe9a0c09614b74ff90f4ffd979a8412956a812c
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-11-07 10:41:17 +0530

    change

---
+++ 7_Nov/PayForPhones.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class PayForPhones {
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        
+        int price[] = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int qui[]=Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        
+        Long sum=0L;
+        
+        for(int i=0;i<price.length;i++){
+            sum+=price[i]*qui[i];
+        }
+        
+        if(sum<150000)System.out.print("Possible");
+        else System.out.print("Not Possible");
+        
+    }
+    
+}
