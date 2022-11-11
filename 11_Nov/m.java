863308eab6f7a914023131b3dce64950ae16263d
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-11-11 22:47:52 +0530

    change

---
+++ 11_Nov/m.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class m{
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        
+        int n= sc.nextInt();
+        
+        int val=(n*10)+2-11;
+        System.out.println(val);
+    }
+}
+
