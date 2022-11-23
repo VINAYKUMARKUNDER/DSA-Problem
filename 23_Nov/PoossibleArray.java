8c01f8a23277f63a1f5150cdb0ce368d30851bd2
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-11-23 14:52:36 +0530

    change

---
+++ 23_Nov/PoossibleArray.java
@@ -0,0 +1,42 @@
+
+
+public class PoossibleArray {
+    public static void masaiPalindromicSubstring(String str){
+        int n=str.length();
+        int max=0;
+        char ch [] =str.toCharArray();
+        
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                
+                String s="";
+                for(int k=i;k<=j;k++){
+                    s+=(ch[k]);
+                }
+                 boolean st= palindrome(s);
+                 if(st){
+                     int len = s.length();
+                     max=Math.max(len,max);
+                 }
+            }
+        }
+        
+        System.out.println(max);
+      
+    }
+    
+    public static boolean palindrome(String s){
+        char ch[]=s.toCharArray();
+        int n=ch.length;
+        
+        for(int i=0,j=n-1; i<n && j>=0; i++,j--){
+            
+            if(ch[i]!=ch[j])return false;
+           
+        }
+        return true;
+    }
+    
+}
+
+
