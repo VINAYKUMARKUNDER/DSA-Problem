cd59bb92438907cbfa41a5e50f48e557de202c03
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-11-30 11:16:11 +0530

    change

--- 30_Nov/GenrateSubString.java
+++ 30_Nov/GenrateSubString.java
@@ -4,6 +4,30 @@ import java.util.*;
 public class GenrateSubString{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
         
         int tc = sc.nextInt();
         for(int i=0;i<tc;i++){
