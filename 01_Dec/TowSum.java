e73a4bd4f7b1b8f4b121aa36f331f0b53a7e2d97
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-12-01 22:43:14 +0530

    change

---
+++ 01_Dec/TowSum.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class TowSum {
+      
+        public static void main(String [] args){
+            Scanner sc=new Scanner(System.in);
+            
+            int tc=sc.nextInt();
+            
+            for(int i=0;i<tc;i++){
+                
+                int n=sc.nextInt();
+                int k=sc.nextInt();
+                
+                sc.nextLine();
+                
+                int arr[]= Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                
+                List<Integer> ans=findSum(arr,n,k);
+                
+                System.out.println(ans.get(0)+" "+ans.get(1));
+            }
+        }
+        
+        static public List<Integer> findSum(int [] arr, int n, int k){
+            List<Integer> list=new ArrayList<>();
+            
+            int start=0;
+            int end=1;
+            
+            while(start<end && end<n){
+                int sum=arr[start]+arr[end];
+                
+                if(sum==k){
+                    list.add(start);
+                    list.add(end);
+                    break;
+                }
+                
+                
+                if(end==n-1){
+                    start++;
+                    end=start+1;
+                    
+                }
+                else end++;
+                
+                
+                
+            }
+            // Long sum=0l;
+
+           
+            
+            if(list.size()==0){
+                list.add(-1);
+                list.add(-1);
+                
+            }
+            
+            return list;
+        }
+    
+}
