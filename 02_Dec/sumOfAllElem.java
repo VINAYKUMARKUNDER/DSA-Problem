3a5e6c7e116419331589cbd74865c0928f1e8eec
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-12-02 23:29:38 +0530

    change

---
+++ 02_Dec/sumOfAllElem.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class sumOfAllElem{
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enetr a number..");
+
+        int n=sc.nextInt();
+
+
+
+
+
+
+        
+        sc.nextLine();
+
+        int arr[]=Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sum=0;
+        for(int a:arr)sum+=a;
+
+        System.out.println(sum);
+    }
+}
