be7c302b4bcd07f7f71442111f324f7bcc5a4481
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-01-26 22:29:48 +0530

    change

--- Python/variable.py
+++ Python/variable.py
@@ -2,3 +2,12 @@ a="vinay" #String
 b=30   #number
 c=40.33  # float
 d='d'  # charecter
+
+#variable:- variable is one type container this is store particular value
+# u can see a is store a spesfic value same as b, c, d also this is all diff- diff data-type.
+
+#Data-type:- Data-type is one type spesfic class u can say.
+
+# keyword:-  keyword is resvre variable. this is pre-difined variable.
+
+# string can write '', "", ''' ''' also.
